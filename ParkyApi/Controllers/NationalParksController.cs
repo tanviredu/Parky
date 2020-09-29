4991f64bb2ecf9d6e486a49b2609583d4ee460a7
zakariabijoy <zakaria@vonome.com>
2020-09-29 18:58:18 +0600

    national park api controller -- In Parky Api

---
+++ ParkyApi/Controllers/NationalParksController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ParkyApi.Repository.IRepository;
+
+namespace ParkyApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NationalParksController : ControllerBase
+    {
+        private readonly INationalParkRepository _npRepo;
+        private readonly IMapper _mapper;
+
+        public NationalParksController(INationalParkRepository npRepo, IMapper mapper)
+        {
+            _npRepo = npRepo;
+            _mapper = mapper;
+        }
+    }
+}
