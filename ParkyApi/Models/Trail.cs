3b6bb8f344dd843374a262fa2b55850baa370f2d
zakariabijoy <zakaria@vonome.com>
2020-10-02 00:01:30 +0600

    Trail Model and Trail Dto   --In ParkyApi

---
+++ ParkyApi/Models/Trail.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyApi.Models
+{
+    public class Trail
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public double Distance { get; set; }
+        public DifficultyType Difficulty { get; set; }
+
+        [Required]
+        public int NationalParkId { get; set; }
+        [ForeignKey("NationalParkId")]
+        public NationalPark NationalPark { get; set; }
+        public DateTime Date { get; set; }
+
+
+
+
+    }
+
+    public enum DifficultyType{
+        Easy,
+        Moderate,
+        Difficult,
+        Expert
+    }
+}
