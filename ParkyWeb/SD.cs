dc10d763bdf7757c181292c67b41b869e53ddc3b
zakariabijoy <zakaria.bijoy@live.com>
2020-10-05 01:28:57 +0600

    Api Static Details -- In ParkyWeb

---
+++ ParkyWeb/SD.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyWeb
+{
+    public static class SD
+    {
+        public static string APIBaseUrl = "https://localhost:44325/";
+        public static string NationalParkApiPath = APIBaseUrl+ "api/v1/nationalparks";
+        public static string TrailsApiPath = APIBaseUrl +"api/v1/trails";
+
+    }
+}
