220338ffe44be341a46a182f8aee18b9175fc0c8
zakariabijoy <zakaria.bijoy@live.com>
2020-10-05 22:55:02 +0600

    Nationalpark and Trail Repository  --In ParkyWeb

---
+++ ParkyWeb/Repository/NationalparkRepository.cs
@@ -0,0 +1,20 @@
+﻿using ParkyWeb.Models;
+using ParkyWeb.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ParkyWeb.Repository
+{
+    public class NationalparkRepository : Repository<NationalPark>, INationalparkRepository
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public NationalparkRepository(IHttpClientFactory httpClientFactory):base(httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+    }
+}
