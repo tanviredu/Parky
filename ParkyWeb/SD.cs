0987b4ef9092a27802e858b95bcc14c221a1e0d6
zakariabijoy <zakaria.bijoy@live.com>
2020-10-06 21:04:22 +0600

    Upsert View part 1 of national park   -- In ParkyWeb

--- ParkyWeb/SD.cs
+++ ParkyWeb/SD.cs
@@ -8,8 +8,8 @@ namespace ParkyWeb
     public static class SD
     {
         public static string APIBaseUrl = "https://localhost:44325/";
-        public static string NationalParkApiPath = APIBaseUrl+ "api/v1/nationalparks";
-        public static string TrailsApiPath = APIBaseUrl +"api/v1/trails";
+        public static string NationalParkApiPath = APIBaseUrl+ "api/v1/nationalparks/";
+        public static string TrailsApiPath = APIBaseUrl +"api/v1/trails/";
 
     }
 }
