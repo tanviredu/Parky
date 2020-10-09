52915aa95b0ff91c7c84fbbb0510fe649170244a
zakariabijoy <zakaria.bijoy@live.com>
2020-10-09 23:40:14 +0600

    Add User Model and Account Repository -- In ParkyWeb

---
+++ ParkyWeb/Repository/AccountRepository.cs
@@ -0,0 +1,29 @@
+﻿using ParkyWeb.Models;
+using ParkyWeb.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ParkyWeb.Repository
+{
+    public class AccountRepository : Repository<User>, IAccountRepository
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AccountRepository(IHttpClientFactory httpClientFactory):base(httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public Task<User> LoginAsync(string url, User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Register(string url, User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
